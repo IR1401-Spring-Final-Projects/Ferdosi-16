b9bba90480d7548f76a3e45c544f31fe6c3dc10d
Nazanin Azarian <badi.mojgan@gmail.com>
2022-07-27 21:07:12 +0430

    Add View Method

--- web/src/search/views.py
+++ web/src/search/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from client.grpc_client import GrpcClient
+from api import search_pb2, query_expansion_pb2
+from google.protobuf.json_format import MessageToDict
 
 
 class TestClass(View):
     def get(self, request):
+        if "query" in request.GET:
+            query = request.GET["query"]
+            grpc_cli = GrpcClient()
+            query_response = grpc_cli.search_stub.Retrieve(search_pb2.SearchRequest(query=query))
+            query_expansion_response = grpc_cli.query_expansion_stub.Expand(
+                query_expansion_pb2.ExpandRequest(query=query))
+            return render(request, "search/main.html",
+                          MessageToDict(query_response) + MessageToDict(query_expansion_response))
         return render(request, "search/main.html")
